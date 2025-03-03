f2c11e81203bc5c6cea9178984b72027b55d6b12
stamenione <vidrak97@gmail.com>
2025-03-03 14:41:39 +0100

    chore: change credential to something simpler

--- packages/content/src/attributes/types/credential/Credential.ts
+++ packages/content/src/attributes/types/credential/Credential.ts
@@ -1,29 +1,19 @@
-import { serialize, validate } from "@js-soft/ts-serval";
-import { AbstractStringJSON, IAbstractString } from "../AbstractString";
-import { AbstractXML } from "../strings/AbstractXML";
+import { type } from "@js-soft/ts-serval";
+import { AbstractString, AbstractStringJSON, IAbstractString } from "../AbstractString";
 
 export interface CredentialJSON extends AbstractStringJSON {
     "@type": "Credential";
-    schemaURL?: string;
 }
-export interface ICredential extends IAbstractString {
-    schemaURL?: string;
-}
-export declare class Credential extends AbstractXML implements ICredential {
-    @serialize()
-    @validate({
-        nullable: true,
-        min: 3,
-        max: 1024,
-        regExp: new RegExp(".*")
-    })
-    public schemaURL?: string;
 
-    public static from(value: Credential | Omit<Credential, "@type"> | string): Credential {
+export interface ICredential extends IAbstractString {}
+
+@type("Credential")
+export class Credential extends AbstractString implements ICredential {
+    public static from(value: ICredential | Omit<CredentialJSON, "@type"> | string): Credential {
         return this.fromAny(value);
     }
 
-    public override toJSON(verbose?: boolean | undefined, serializeAsString?: boolean | undefined): Credential {
-        return super.toJSON(verbose, serializeAsString) as Credential;
+    public override toJSON(verbose?: boolean | undefined, serializeAsString?: boolean | undefined): CredentialJSON {
+        return super.toJSON(verbose, serializeAsString) as CredentialJSON;
     }
 }
